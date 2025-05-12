abe0bffe19268799a4f865391abe34a14682f96f
Santiago Ovalle <133941360+Sovalle93@users.noreply.github.com>
2025-05-12 15:32:03 -0400

    Update page.js
    removing redundancies

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,15 @@
-import Head from 'next/head'
-import Hero from './components/Hero'
-import ImageCarousel from './components/ImageCarousel'
-import DemoVideoPlayer from './components/DemoVideoPlayer'
-
+import Hero from './components/Hero';
+import ImageCarousel from './components/ImageCarousel';
+import DemoVideoPlayer from './components/DemoVideoPlayer';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Studio Apolo | Soluciones Creativas</title>
-        <meta name="description" content="Studio Apolo - Innovative solutions for your business" />
-      </Head>
       <main className="min-h-screen bg-white">
-        {/* Hero Section */}
         <Hero />
-
-        {/* Image Carousel Section */}
         <ImageCarousel />
-
-        {/* Demo Video Player Section */}
         <DemoVideoPlayer />
-
       </main>
     </>
-  )
+  );
 }
